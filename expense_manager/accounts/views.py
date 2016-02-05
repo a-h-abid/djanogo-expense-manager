e4a4f4850442842a892723c94280887aa9ff0fde
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2016-02-05 13:22:21 +0000

    Details, and List of Transaction done.

--- expense_manager/accounts/views.py
+++ expense_manager/accounts/views.py
@@ -1,59 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Account, Transaction
-from .repositories.account import Account as AccountRepo 
+from .repositories.account_repo import AccountRepo 
 
+@login_required
 def index(request):
-    accounts = Account.objects.filter(id=1)
-    context = {'accounts': accounts}
+    accounts = Account.objects.filter(user_id= request.user.id)
+    context = {'accounts' : accounts}
     return render(request, 'accounts/accounts/list.html', context)
-    
+
+@login_required
 def create(request):
     context = {'form_mode' : 'add'}
     return render(request, 'accounts/accounts/form.html', context)
     
+@login_required
 def edit(request, account_id):
     account = Account.objects.get(id=account_id)
     context = {
-        'account': account,
+        'account' : account,
         'form_mode' : 'edit'
     }
     return render(request, 'accounts/accounts/form.html', context)
     
+@login_required
 def detail(request, account_id):
-    account = Account.objects.get(id=account_id)
-    #acc = AccountRepo()
-    #account = acc.get(id=account_id)
-    account.balance = 30000
+    account = AccountRepo().get(account_id)
     context = {
-        'account': account,
-        'balance_format' : account.currency + ' ' + str(account.balance)
+        'account' : account,
+        'starting_amount_format' : account.currency + ' ' + str(account.starting_amount),
+        'balance_format' : account.currency + ' ' + str(account.balance),
     }
     return render(request, 'accounts/accounts/detail.html', context)
     
+@login_required
 def list_transaction(request, account_id):
-    account = Account.objects.get(id=account_id)
+    account = AccountRepo().get(account_id)
     transactions = Transaction.objects.filter(account_id=account.id)
     context = {
-        'account': account,
-        'transactions' : transactions
+        'account' : account,
+        'transactions' : account.transactions
     }
     return render(request, 'accounts/transactions/list.html', context)
     
+@login_required
 def add_transaction(request, account_id):
-    account = Account.objects.get(id=account_id)
+    account = AccountRepo().get(account_id)
     context = {
-        'account': account, 
+        'account' : account, 
         'form_mode' : 'add'
     }
     return render(request, 'accounts/transactions/form.html', context)
 
+@login_required
 def edit_transaction(request, account_id, transaction_id):
-    account = Account.objects.get(id=account_id)
+    account = AccountRepo().get(account_id)
     transaction = Transaction.objects.get(id=transaction_id)
     context = {
-        'account': account, 
+        'account' : account, 
         'transaction' : transaction, 
         'form_mode' : 'edit'
     }
     return render(request, 'accounts/transactions/form.html', context)
+
+
